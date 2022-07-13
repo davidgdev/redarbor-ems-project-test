ea4c9c04696aa1e94a3cffb24dff4a8a8cdb3c24
David Galeano <davidgaleano217@gmail.com>
2022-07-13 18:06:00 -0500

    Fixing deployement

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className="bg-neutral-900 ">
-      <div className="px-10 container flex flex-col min-h-screen m-auto py-4">
+    <div className="bg-neutral-900 min-h-screen flex items-center">
+      <div className="px-10 container min-h-screen m-auto py-4">
         <PostProvider>
           <Navbar />
           <Routes>
